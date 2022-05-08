51a1bb07f4c849b12cfe7aa0834799a45c7c87ae
n-sassa <sshr2943@gmail.com>
2022-05-09 08:06:40 +0900

    initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { BadgeCheckIcon } from '@heroicons/react/solid'
+
+interface Props {
+	title: string
+	children: React.ReactNode
+}
+
+export const Layout: React.FC<Props> = ({ children, title = 'Todo app' }) => {
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center font-mono text-gray-800">
+			<Head>
+				<title>{title}</title>
+			</Head>
+			<header></header>
+			<main className="flex w-screen flex-1 flex-col items-center justify-center">
+				{children}
+			</main>
+			<footer className="flex h-12 w-full items-center justify-center border-t">
+				<BadgeCheckIcon className="h-6 w-6 text-blue-500" />
+			</footer>
+		</div>
+	)
+}
---
+++ pages/ssr.tsx
@@ -0,0 +1,64 @@
+import { GetServerSideProps, NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Layout } from '../components/Layout'
+import { Notice, Task } from '../types/types'
+import { supabase } from '../utils/supabase'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	console.log('getServerSideProps/ssr invoked')
+
+	const { data: tasks } = await supabase
+		.from('todos')
+		.select('*')
+		.order('created_at', { ascending: true })
+
+	const { data: notices } = await supabase
+		.from('notices')
+		.select('*')
+		.order('created_at', { ascending: true })
+
+	return { props: { tasks, notices } }
+}
+
+type ServerSideProps = {
+	tasks: Task[]
+	notices: Notice[]
+}
+
+const Ssr: NextPage<ServerSideProps> = ({ tasks, notices }) => {
+	const router = useRouter()
+	return (
+		<Layout title="SSR">
+			<p className="mb-3 text-blue-500">SSR</p>
+			<ul className="mb-3">
+				{tasks.map((task) => (
+					<li key={task.id}>
+						<p className="text-lg font-extrabold">{task.title}</p>
+					</li>
+				))}
+			</ul>
+			<ul className="mb-3">
+				{notices.map((notice) => (
+					<li key={notice.id}>
+						<p className="text-lg font-extrabold">{notice.content}</p>
+					</li>
+				))}
+			</ul>
+			<Link href="/ssg" prefetch={false}>
+				<a className="my-3 text-xs">Link to ssg</a>
+			</Link>
+			<Link href="/isr" prefetch={false}>
+				<a className="mb-3 text-xs">Link to isr</a>
+			</Link>
+			<button className="mb-3 text-xs" onClick={() => router.push('/ssg')}>
+				Route to ssg
+			</button>
+			<button className="mb-3 text-xs" onClick={() => router.push('/isr')}>
+				Route to isr
+			</button>
+		</Layout>
+	)
+}
+
+export default Ssr
